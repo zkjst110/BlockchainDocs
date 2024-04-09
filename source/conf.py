9add99e28dc76b78baa853b44775f980b53055bf
zkjst110 <32483418+zkjst110@users.noreply.github.com>
2024-04-09 14:27:16 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = 'V0.0.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +26,8 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+ extensions = [
+     'recommonmark',
+     'sphinx_markdown_tables'
+ ]
